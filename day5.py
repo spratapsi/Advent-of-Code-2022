32682ff0635718eb4a12a769e8138a1c3b76cd76
Sagar Pratapsi <sagar.pratapsi@meaningfuldata.eu>
2022-12-06 00:27:23 +0000

    correction

--- day5.py
+++ day5.py
@@ -7,21 +7,20 @@ with open('day5.in') as file:
     for line in file:
         if not line.strip():
             break
-        stacks.append(line.rstrip()[1::4])
+        stacks.append(line[1::4])
     stacks = [list(''.join(reversed(col)).rstrip()) for col in zip(*stacks)]
 
-    template_instruction = re.compile(r'move (\d+) from (\d) to (\d)')
     for line in file:
-        m = re.match(template_instruction, line.rstrip())
-        if m:
-            instructions.append([int(x) for x in m.groups()])
+        match = re.match(r'move (\d+) from (\d) to (\d)', line.rstrip())
+        if match:
+            instructions.append([int(x) for x in match.groups()])
 
 def move(stacks, instructions, reverse=True):
     stacks = deepcopy(stacks)
-    for qty, source, target in instructions:
-        to_move = stacks[source-1][-1:-qty-1:-1]
+    for ncrates, source, target in instructions:
+        to_move = stacks[source-1][-1:-ncrates-1:-1]
         stacks[target-1] += to_move if reverse else to_move[::-1]
-        del stacks[source-1][-1:-qty-1:-1]
+        del stacks[source-1][-1:-ncrates-1:-1]
     return stacks
 
 stacks1 = move(stacks, instructions, reverse=True)
